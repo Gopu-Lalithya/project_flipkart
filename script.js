4c575d2489f1899e19cf4f2d37967974bcb6d759
Gopu-Lalithya <lalithyakumar2001@gmail.com>
2022-10-11 22:06:44 +0530

    added coupon

---
+++ script.js
@@ -0,0 +1,10 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('slider').style.opacity='0.7';
+    document.getElementById('couponContents').style.opacity='1'
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('slider').style.opacity='1'
+}
